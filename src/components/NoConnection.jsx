86eb855473eda564f43d6efbfd33fb92dd13de54
Siddhant Gupta <me@guptasiddhant.com>
2018-03-20 16:29:08 +0100

    No Connection

---
+++ src/components/NoConnection.jsx
@@ -0,0 +1,11 @@
+/**
+ * Created by Siddhant Gupta on 2018-03-20.
+ */
+
+import React from "react";
+
+export default function NoConnection() {
+    return <div className="no-connection">
+        <h5>⚠️ Error! No connection found</h5>
+    </div>;
+}
